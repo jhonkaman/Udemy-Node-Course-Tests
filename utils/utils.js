ab9da78ad46f7c3325641cef78a7c75a1388ebb0
javier alvarado <jdrummer1@outlook.com>
2017-05-04 11:29:16 -0700

    Video 51
    Mocha and Basic Testing

---
+++ utils/utils.js
@@ -0,0 +1,4 @@
+module.exports.add = (a, b) => a+ b + 4; //<- Putting on one line means it's implicitly returned
+// ^ Add a number at the end to make it fail the test (i.e., + 22)
+module.exports.square = (x) => x * x;
+// ^ Add a number at the end to make it fail the test (i.e., + 22)
---
+++ utils/utils.test.js
@@ -0,0 +1,20 @@
+const utils = require('./utils');
+
+it('should add two numbers', () => {
+  var res = utils.add(33, 11);
+
+  if (res !== 44) {
+    throw new Error(`Expected 44 but got ${res}.`)
+  }
+  //throw new Error('Value not correct');
+});
+
+it('should square a number', () => {
+  var res = utils.square(11);
+
+  if (res !== 121) {
+    throw new Error(`Expected 121 but got ${res}.`);
+  }
+});
+
+// Use nodemon --exec 'npm test' to run test
